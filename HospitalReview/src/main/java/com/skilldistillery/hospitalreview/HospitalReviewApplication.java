f65652c33d5f13400cc469a6cb2fd3ee4549bac0
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-03-07 12:43:03 -0700

    Project stubs including README

---
+++ HospitalReview/src/main/java/com/skilldistillery/hospitalreview/HospitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hospitalreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalReviewApplication.class, args);
+	}
+
+}
