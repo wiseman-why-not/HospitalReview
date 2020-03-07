cc5dc23c089004ed220a0382813ac8eb79a4b5be
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-03-07 13:43:46 -0700

    stubbed out the angular portion of the hospital review

--- JPAHospitalReview/src/main/java/entities/User.java
+++ JPAHospitalReview/src/main/java/entities/User.java
@@ -1,5 +1,23 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinTable;
+
+@Entity
+@Table(name="user")
 public class User {
 	
 	// F I E L D S
