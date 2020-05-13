984e2c037bfd3243538418e31040cefa70105d97
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-05-13 14:54:56 -0500

    created landing page and footer component.

--- ngHospitalReview/src/app/app.module.ts
+++ ngHospitalReview/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
