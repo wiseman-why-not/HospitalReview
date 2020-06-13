816b4fd3b0ceb31146356a1954944249d8849a8c
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-06-12 21:14:32 -0500

    added jpa repository searches for post. Still have to test them to see if they are retrieving the right information.:

--- HospitalReview/src/main/java/com/skilldistillery/hospitalreview/repositories/HospitalRepository.java
+++ HospitalReview/src/main/java/com/skilldistillery/hospitalreview/repositories/HospitalRepository.java
@@ -11,4 +11,5 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 	Hospital findByName(String hospitalName);
 	
 	List<Hospital> findByNameContaining(String hospitalName);
+	
 }
--- HospitalReview/src/main/java/com/skilldistillery/hospitalreview/repositories/PostRepository.java
+++ HospitalReview/src/main/java/com/skilldistillery/hospitalreview/repositories/PostRepository.java
@@ -1,9 +1,33 @@
 package com.skilldistillery.hospitalreview.repositories;
+/* 
+ * Spring Data JPA in particular allows us to get some of the basic 
+ * necessities for any app (CRUD functionality) without writing any 
+ * implementation code.
+ * 
+ * JpaRepository gives us queries by using a 
+ * naming convention. useful for aggregating 
+ * data over the entity represented by the repository.
+ */
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.hospitalreview.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+	// To query by properties of objects contained in an entity, 
+	// use underscore (_).
+	
+	// SELECT * FROM Post WHERE hospital_id = (some number);
+	List<Post> findByHospital_id(Integer id);
+	
+	// SELECT * FROM Post WHERE medicalprocedure_id = (some number);
+	List<Post> findByMedicalprocedure_id(Integer id);
+	
+	// SELECT * FROM Post WHERE user_id = (some number);
+	List<Post> findByUser_id(Integer id);
 }
